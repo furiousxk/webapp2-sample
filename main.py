5f2435769b55aa6b54b0e0c62e685fbdc0758efb
furiousxk <nickolas.dury@protonmail.com>
2019-01-03 09:29:59 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import webapp2
+import json
+
+class HelloWebApp2(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['Content-Type'] = 'application/json'
+		response = {
+			'message': 'Hello, webapp2!'
+		}
+		self.response.out.write(json.dumps(response))
+
+app = webapp2.WSGIApplication([
+	('/', HelloWebApp2),
+], debug=True)
+
+def main():
+	from paste import httpserver
+	httpserver.serve(app, host='127.0.0.1', port='9090')
+
+if __name__ == '__main__':
+	main()
